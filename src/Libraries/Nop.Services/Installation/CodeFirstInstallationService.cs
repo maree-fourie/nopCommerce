fd7a353188ade7028aee9e34948c9de7e90ed61d
DmitriyKulagin <kulagin87@gmail.com>
2021-02-20 14:40:30 +0300

    Issue 5290 Minor fix

--- src/Libraries/Nop.Services/Installation/CodeFirstInstallationService.cs
+++ src/Libraries/Nop.Services/Installation/CodeFirstInstallationService.cs
@@ -674,7 +674,8 @@ namespace Nop.Services.Installation
             var pattern = "*.txt";
 
             //we use different scope to prevent creating wrong settings in DI, because the settings data not exists yet
-            using var scope = EngineContext.Current.Resolve<IServiceProvider>().CreateScope();
+            var serviceScopeFactory = EngineContext.Current.Resolve<IServiceScopeFactory>();
+            using var scope = serviceScopeFactory.CreateScope();
             var importManager = EngineContext.Current.Resolve<IImportManager>(scope);
             foreach (var filePath in _fileProvider.EnumerateFiles(directoryPath, pattern))
             {
